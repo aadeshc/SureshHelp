313cd157e954d7bed3fdbf8d978b8cfe268137c5
Snehal <isnehalr@gmail.com>
2020-07-30 19:19:02 +0530

    updates

--- AuthorQuiz-react-redux-master/src/AddAuthorForm.js
+++ AuthorQuiz-react-redux-master/src/AddAuthorForm.js
@@ -1,73 +1,130 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
-import './AddAuthorForm.css';
+import React, { useContext, useState, useReducer } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import UserContext from "./UserContext";
+import "./AddAuthorForm.css";
+import { reducer } from "./AuthorQuiz";
 
-class AuthorForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            imageUrl: '',
-            books: [],
-            bookTemp: ''
-        };
-        this.onFieldChange = this.onFieldChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleAddBook = this.handleAddBook.bind(this);
-    }
-    handleSubmit(event) {
-        event.preventDefault();
-        this.props.onAddAuthor(this.state);
-    }
-    onFieldChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-    }
-    handleAddBook(event) {
-        this.setState({
-            books: this.state.books.concat([this.state.bookTemp]), 
-            bookTemp: ''
-        })
-    }
-    render() {
-        return(
-            <form onSubmit={this.handleSubmit}>
-                <div className="AddAuthorForm__input">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" name="name" value={this.state.name} onChange={this.onFieldChange}/>
-                </div>
-                <div className="AddAuthorForm__input">
-                    <label htmlFor="imageUrl">Image URL</label>
-                    <input type="text" name="imageUrl" value={this.state.imageUrl} onChange={this.onFieldChange}/>
-                </div>
-                <div className="AddAuthorForm__input">
-                    {this.state.books.map((book) => <p key={book}>{book}</p>)}
-                    <label htmlFor="bookTemp">Books</label>
-                    <input type="text" name="bookTemp" value={this.state.bookTemp} onChange={this.onFieldChange}/>
-                    <input type="button" value="+" onClick={this.handleAddBook} />
-                </div>
-                <input type="submit" value="Add" />
-            </form>
-        );
-    }
-}
+const AuthorForm = (props) => {
+  // constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //         name: '',
+  //         imageUrl: '',
+  //         books: [],
+  //         bookTemp: ''
+  //     };
+  //     this.onFieldChange = this.onFieldChange.bind(this);
+  //     this.handleSubmit = this.handleSubmit.bind(this);
+  //     this.handleAddBook = this.handleAddBook.bind(this);
+  // }
 
-function AddAuthorForm({match, onAddAuthor}) {
-    return <div className="AddAuthorForm">
-        <h1>Add Author</h1>
-        <AuthorForm onAddAuthor={onAddAuthor}/>
-    </div>;
-}
+  //  const [state, dispatch] = useContext(UserContext);
+  const [name, setName] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [books, setBooks] = useState([]);
+  const [bookTemp, setBookTemp] = useState("");
+
+  const [state, dispatch] = useReducer(reducer, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.onAddAuthor(this.state);
+  };
+  const onFieldChange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    if (name === "name") {
+      setName(value);
+    }
+    if (name === "imageUrl") {
+      setImageUrl(value);
+    }
+    if (name === "bookTemp") {
+      setBookTemp(value);
+    }
+
+    // this.setState({
+    //     [event.target.name]: event.target.value
+    // });
+  };
+  const handleAddBook = (event) => {
+    setBooks(books.concat([bookTemp]));
+    setBookTemp("");
+    // this.setState({
+    //     books: this.state.books.concat([this.state.bookTemp]),
+    //     bookTemp: ''
+    // })
+  };
+  // render() {
+  return (
+    <form onSubmit={() => handleSubmit}>
+      <div className="AddAuthorForm__input">
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" value={name} onChange={onFieldChange} />
+      </div>
+      <div className="AddAuthorForm__input">
+        <label htmlFor="imageUrl">Image URL</label>
+        <input
+          type="text"
+          name="imageUrl"
+          value={imageUrl}
+          onChange={onFieldChange}
+        />
+      </div>
+      <div className="AddAuthorForm__input">
+        {books.map((book) => (
+          <p key={book}>{book}</p>
+        ))}
+        <label htmlFor="bookTemp">Books</label>
+        <input
+          type="text"
+          name="bookTemp"
+          value={bookTemp}
+          onChange={onFieldChange}
+        />
+        <input
+          type="button"
+          value="+"
+          onClick={() => {
+            setBooks(books.concat([bookTemp]));
+            setBookTemp("");
+          }}
+        />
+      </div>
+      <input
+        type="submit"
+        value="Add"
+        onClick={() => {
+          dispatch({ type: "ADD_AUTHOR", author: { name, imageUrl, books } });
+          props.history.push("/");
+        }}
+      />
+    </form>
+  );
+  // }
+};
+
+// <UserContext.Consumer>
+//   {(value) => <AddAuthorForm {...value} />}
+// </UserContext.Consumer>;
+
+export const AddAuthorForm = (props) => {
+  return (
+    <div className="AddAuthorForm">
+      <h1>Add Author</h1>
+      <AuthorForm {...props} />
+    </div>
+  );
+};
 
 function mapDispatchToProps(dispatch, props) {
-    return {
-        onAddAuthor: (author) => {
-            dispatch({ type: 'ADD_AUTHOR', author });
-            props.history.push('/');
-        }
-    };
+  return {
+    onAddAuthor: (author) => {
+      dispatch({ type: "ADD_AUTHOR", author });
+      props.history.push("/");
+    },
+  };
 }
-  
-export default withRouter(connect( ()=> {}, mapDispatchToProps)(AddAuthorForm));
+
+export default withRouter(AddAuthorForm);
--- AuthorQuiz-react-redux-master/src/index.js
+++ AuthorQuiz-react-redux-master/src/index.js
@@ -1,55 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, withRouter } from 'react-router-dom';
-import * as Redux from 'redux';
-import * as ReactRedux from 'react-redux';
-import './index.css';
-import AuthorQuiz from './AuthorQuiz';
-import AddAuthorForm from './AddAuthorForm';
-import * as serviceWorker from './serviceWorker';
-import { shuffle, sample } from 'underscore';
-import { UserProvider } from './UserContext'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, withRouter } from "react-router-dom";
+import * as Redux from "redux";
+import * as ReactRedux from "react-redux";
+import "./index.css";
+import AuthorQuiz from "./AuthorQuiz";
+import AddAuthorForm from "./AddAuthorForm";
+import * as serviceWorker from "./serviceWorker";
+import { shuffle, sample } from "underscore";
+import { UserProvider } from "./UserContext";
 const authors = [
   {
-    name: 'Mark Twain',
-    imageUrl: 'images/authors/marktwain.jpg',
-    books: ['The Adventures of Huckleberry Finn']
+    name: "Mark Twain",
+    imageUrl: "images/authors/marktwain.jpg",
+    books: ["The Adventures of Huckleberry Finn"],
   },
   {
-    name: 'Joseph Conrad',
-    imageUrl: 'images/authors/josephconrad.jpg',
-    books: ['Heart of Darkness']
+    name: "Joseph Conrad",
+    imageUrl: "images/authors/josephconrad.jpg",
+    books: ["Heart of Darkness"],
   },
   {
-    name: 'J.K. Rowling',
-    imageUrl: 'images/authors/jkrowling.jpg',
-    imageSource: 'Wikimedia Commons',
-    imageAttribution: 'Daniel Ogren',
-    books: ['Harry Potter and the Sorcerers Stone']
+    name: "J.K. Rowling",
+    imageUrl: "images/authors/jkrowling.jpg",
+    imageSource: "Wikimedia Commons",
+    imageAttribution: "Daniel Ogren",
+    books: ["Harry Potter and the Sorcerers Stone"],
   },
   {
-    name: 'Stephen King',
-    imageUrl: 'images/authors/stephenking.jpg',
-    imageSource: 'Wikimedia Commons',
-    imageAttribution: 'Pinguino',
-    books: ['The Shining', 'IT']
+    name: "Stephen King",
+    imageUrl: "images/authors/stephenking.jpg",
+    imageSource: "Wikimedia Commons",
+    imageAttribution: "Pinguino",
+    books: ["The Shining", "IT"],
   },
   {
-    name: 'Charles Dickens',
-    imageUrl: 'images/authors/charlesdickens.jpg',
-    imageSource: 'Wikimedia Commons',
-    books: ['David Copperfield', 'A Tale of Two Cities']
+    name: "Charles Dickens",
+    imageUrl: "images/authors/charlesdickens.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["David Copperfield", "A Tale of Two Cities"],
   },
   {
-    name: 'William Shakespeare',
-    imageUrl: 'images/authors/williamshakespeare.jpg',
-    imageSource: 'Wikimedia Commons',
-    books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+    name: "William Shakespeare",
+    imageUrl: "images/authors/williamshakespeare.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["Hamlet", "Macbeth", "Romeo and Juliet"],
   },
-]
+];
 
 function getTurnData(authors) {
-  const allBooks = authors.reduce(function (p, c, i) {
+  const allBooks = authors.reduce(function(p, c, i) {
     return p.concat(c.books);
   }, []);
   const fourRandomBooks = shuffle(allBooks).slice(0, 4);
@@ -58,43 +58,50 @@ function getTurnData(authors) {
   return {
     books: fourRandomBooks,
     author: authors.find((author) =>
-      author.books.some((title) =>
-        title === answer))
-  }
+      author.books.some((title) => title === answer)
+    ),
+  };
 }
 
-function reducer(state = { authors, turnData: getTurnData(authors), highlight: '' }, action) {
+function reducer(
+  state = { authors, turnData: getTurnData(authors), highlight: "" },
+  action
+) {
   switch (action.type) {
-    case 'ANSWER_SELECTED':
-      const isCorrect = state.turnData.author.books.some((book) => book === action.answer);
-      return Object.assign({}, state, { highlight: isCorrect ? 'correct' : 'wrong' });
-    case 'CONTINUE':
-      return Object.assign({}, state, { highlight: '', turnData: getTurnData(state.authors) });
-    case 'ADD_AUTHOR':
-      return Object.assign({}, state, { authors: state.authors.concat([action.author]) });
-    default: return state;
+    case "ANSWER_SELECTED":
+      const isCorrect = state.turnData.author.books.some(
+        (book) => book === action.answer
+      );
+      return Object.assign({}, state, {
+        highlight: isCorrect ? "correct" : "wrong",
+      });
+    case "CONTINUE":
+      return Object.assign({}, state, {
+        highlight: "",
+        turnData: getTurnData(state.authors),
+      });
+    case "ADD_AUTHOR":
+      return Object.assign({}, state, {
+        authors: state.authors.concat([action.author]),
+      });
+    default:
+      return state;
   }
 }
 
 let store = Redux.createStore(reducer);
 
-
-
-
 ReactDOM.render(
-  <BrowserRouter>
-    <ReactRedux.Provider store={store}>
-      <UserProvider value={authors}>
-        <React.Fragment>
-          <Route exact path="/" component={AuthorQuiz} />
-          <Route path="/add" component={AddAuthorForm} />
-
-        </React.Fragment>
-      </UserProvider>
-    </ReactRedux.Provider>
-  </BrowserRouter>, document.getElementById('root'));
-
-
+  <UserProvider value={authors}>
+    <BrowserRouter>
+      <React.Fragment>
+        <Route exact path="/" component={AuthorQuiz} />
+        <Route path="/add" component={AddAuthorForm} />
+      </React.Fragment>
+    </BrowserRouter>
+  </UserProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
